bc4c945653c3d621897f50fe075f1d6ca2bc9f4b
linarias8 <88446847+linarias8@users.noreply.github.com>
2023-12-04 17:49:30 -0500

    Update text

--- IDBsocialdata/Render.R
+++ IDBsocialdata/Render.R
@@ -7,3 +7,6 @@ bookdown::render_book("index.Rmd", "bookdown:::bs4_book")
 #```{r IDB, out.width='50%', fig.align='center', echo=FALSE, include=identical(knitr:::pandoc_to(), 'html'), fig.link='https://www.iadb.org/en/sharing-knowledge/data/social-data', fig.show='hold',  out.extra='style="float:right; padding:0px"'}
 #knitr::include_graphics('figure/IDB.png')
 #```
+
+#date: "2023"
+#author: "Inter-American Development Bank - IDB"
